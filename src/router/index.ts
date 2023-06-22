1733a77165f5b99b74eaea848139f058394a2639
Valentyn Bondarenko <murica.it@gmail.com>
2023-06-22 17:27:37 +0200

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import CashierLogin from '../views/CashierLogin.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Cashier - Login',
+      component: () => import('../views/CashierLogin.vue')
+    },
+    {
+      path: '/login-manager',
+      name: 'Manager - Login',
+      component: () => import('../views/ManagerLogin.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/CashierLogin.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <p>{{ count }}</p>
+  </main>
+</template>
