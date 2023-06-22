12b42effc45adcd39901d426f0576019694ad68a
Valentyn Bondarenko <murica.it@gmail.com>
2023-06-22 18:44:11 +0200

    Add 2 login pages

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+export default {
+  data() {
+  return {
+    id_employee: "",
+    password: "",
+  }
+ },
+ props: {
+    message: String,
+  },
+  methods: {
+    alertForm() {
+      alert(this.id_employee + this.password);
+      this.id_employee = "";
+      this.password = "";
+    }
+  }
+}
+</script>
+
+<template>
+  <v-card class="login-form pb-5 pt-5" width="400">
+      <v-card-item>
+        <v-card-title class="text-center">{{message}}</v-card-title>
+      </v-card-item>
+      <v-card-item>
+        <v-form @submit.prevent>
+      <v-text-field
+        v-model="id_employee"
+        label="Employee ID"
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        label="Password"
+      ></v-text-field>
+      <v-btn type="submit" @click="alertForm" block class="mt-2">Submit</v-btn>
+      </v-form>
+      </v-card-item>
+    </v-card>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/CashierLogin.vue
+++ src/views/CashierLogin.vue
@@ -1,15 +1,74 @@
 <script lang="ts">
+import LoginForm from '../components/LoginForm.vue'
+import { RouterLink } from "vue-router"
+
 export default {
   data() {
     return {
       count: 0
     }
+  },
+  components:  {
+    LoginForm,
   }
 }
 </script>
 
 <template>
-  <main>
-    <p>{{ count }}</p>
+  <main class="d-flex bg-surface-variant">
+    <div class="sheet-block bg-grey-lighten-2">
+      <v-card class="greeting-text bg-grey-lighten-2 elevation-0" width="380">
+        <v-card-item>
+        <v-card-title class="text-h4 font-weight-bold pa-5">Zlagoda grocery</v-card-title>
+      </v-card-item>
+      <v-divider :thickness="2"></v-divider>
+
+      <v-card-text class="text-subtitle-1">
+        Welcome to 'Zlagoda' grocery admin panel. <br>
+        If you want login as a manager of the shop 
+        <RouterLink to="/login-manager">click here</RouterLink>.
+      </v-card-text>
+      </v-card>
+    </div>
+    <div class="sheet-block bg-green-darken-1">
+      <LoginForm class="login-form" message="Login as cashier"/>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height:100vh;
+  position: relative;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  justify-content: space-between;
+  flex-grow: 1;
+}
+
+.greeting-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-20%, -80%);
+}
+
+.sheet-block {
+  width: 50vw;
+  height:100vh;
+  position: relative;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  flex-grow: 1;
+}
+
+.login-form {
+  position: absolute;
+  top: 50%;
+  left:50%;
+  transform: translate(-70%, -50%);
+}
+</style>
